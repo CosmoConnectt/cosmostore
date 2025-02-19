4a1ff56db83df3511776a87f2c34d9299165c42d
CosmoConnectt <projectuse882@gmail.com>
2025-02-20 00:30:00 +0530

    Fixed for Render deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
     baseURL: import.meta.env.MODE === "development" 
         ? "http://localhost:5000/api" 
-        : "https://cosmostore-backend.onrender.com", // Use Render's backend URL in production
+        : "https://cosmostore.onrender.com", // Use Render's backend URL in production
     withCredentials: true, // Send cookies to the server
 });
 
