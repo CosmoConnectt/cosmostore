7654ab55a859889fc7429677c0aba70788575357
CosmoConnectt <projectuse882@gmail.com>
2025-02-20 00:07:30 +0530

    ain

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
-    withCredentials: true, //send cookies to the server
-
-
+    baseURL: import.meta.env.MODE === "development" 
+        ? "http://localhost:5000/api" 
+        : "https://cosmostore-backend.onrender.com", // Use Render's backend URL in production
+    withCredentials: true, // Send cookies to the server
 });
-export default axiosInstance
+
+export default axiosInstance;
